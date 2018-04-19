83e987ffce49332d951ce8e8f591f7b07ef29a76
Krystian Bieniek <krystekk2013@gmail.com>
2018-04-19 14:23:18 +0200

    Add new repository

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var a = prompt('Enter a');
+	b = prompt('Enter b');
+	value = (a * a) - (2 * a * b) - (b * b)
+
+console.log('result is ' + value);
+
+if (value < 0) {
+	console.log('wynik jest ujemny')
+} else if (value > 0) {
+	console.log('wynik jest dodatni')
+} else {
+	console.log('wynik jest zerowy')
+}
+switch (value) {
+	case 0:
+	console.log('wynik zerowy');
+	break;
+	default:
+	console.log('wynik jest inny niz 0')
+}
